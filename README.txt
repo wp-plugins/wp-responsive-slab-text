7fce78757bf4eaa09102fbca456aedcdfa37e161
gal_op <gal_op@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-08 10:12:25 +0000

    Added shortcode parameters/attributes: color, font, transform
    git-svn-id: https://plugins.svn.wordpress.org/wp-responsive-slab-text/trunk@1215608 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- README.txt
+++ README.txt
@@ -1,10 +1,10 @@
 ﻿=== WP Responsive Auto Fit Text ===
 Contributors:gal_op
-Donate link: 
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=2Z366DM56DLHS
 Tags: Fit text, slab text, responsive, shortcode, justify text, slabtext, auto fit text, auto resize text, text resize
 Requires at least: 2.5
-Tested up to: 4.2
-Stable tag: 0.1
+Tested up to: 4.3
+Stable tag: 0.2
 License: GPLv2 or later
 
 WP Responsive Auto Fit Text allows you to create great, big, bold headlines that resize to the viewport width, with a WordPress shortcode.
@@ -22,6 +22,14 @@ Based on slabText – a jQuery plugin for producing big, bold & responsive headl
 [slab]line 4[/slab]
 [/slabtext]`
 
+Styling per line
+`[slabtext]
+[slab color="#000" font="Open Sans" transform="uppercase"]line 1[/slab]
+[slab]line 2[/slab]
+[slab color="#FFF" font="Arial"]line 3[/slab]
+[slab]line 4[/slab]
+[/slabtext]`
+
 [Working Demo](http://www.vibesdesign.com.au/wp-responsive-auto-fit-text-wordpress-plugin/ "WP Responsive Auto Fit Text")
 
 == Frequently Asked Questions ==
@@ -34,8 +42,11 @@ Yes, here is an example:
 </h1>`
 
 == Changelog ==	
-= 1.0 =
+= 0.1 =
 - Initial Revision	
+
+= 0.2 =
+- Added shortcode parameters/attributes: color, font, transform
 
 == Screenshots ==
 There are no screenshots, you can view it live in here: [Working Demo](http://www.vibesdesign.com.au/wp-responsive-auto-fit-text-wordpress-plugin/ "WP Responsive Auto Fit Text")
--- wp-responsive-fit-text.php
+++ wp-responsive-fit-text.php
@@ -3,12 +3,53 @@
 Plugin Name: WP Responsive Auto Fit Text
 Plugin URI: http://www.vibesdesign.com.au/wp-responsive-auto-fit-text-wordpress-plugin/
 Description: WP Responsive Fit Text allows you to create great, big, bold & responsive headlines that resize to the viewport width, using a simple shortcode.
-Version: 0.1
+Version: 0.2
 Author: Gal Opatovsky
 Author URI: http://www.vibesdesign.com.au
 License: GPLv2 or later
 
 */
+add_action('admin_notices', 'cfs_wdc_admin_notice');
+
+function cfs_wdc_admin_notice() {
+	global $current_user ;
+        $user_id = $current_user->ID;
+        /* Check that the user hasn't already clicked to ignore the message */
+	if ( ! get_user_meta($user_id, 'cfs_wdc_ignore_notice') ) {
+        echo '<div class="updated"><p style="float:left;">'; 
+        printf(__('If you like "WP Responsive Auto Fit Text" plugin, please consider making a small donation. Thanks! :) <br> <br> <a href="%1$s">Hide Notice</a>'), '?cfs_wdc_nag_ignore=0');
+        echo "</p>";
+		
+		echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="float:right;">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="NDPJSSZE6KEB6">
+<table>
+<tr><td><input type="hidden" name="on0" value="Select donation amount">Select donation amount</td></tr><tr><td><select name="os0">
+	<option value="Buy me a coffee">Buy me a coffee $5.00 AUD</option>
+	<option value="Buy me a beer">Buy me a beer $10.00 AUD</option>
+	<option value="Motivate me to keep developing Plugins">Motivate me to keep developing Plugins $20.00 AUD</option>
+	<option value="Too generous! Thank you!">Too generous! Thank you! $50.00 AUD</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="AUD">
+<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_AU/i/scr/pixel.gif" width="1" height="1">
+</form>';
+		
+		echo "<div style='clear:both'></div>";
+		echo "</div>";
+	}
+}
+
+add_action('admin_init', 'cfs_wdc_nag_ignore');
+
+function cfs_wdc_nag_ignore() {
+	global $current_user;
+        $user_id = $current_user->ID;
+        if ( isset($_GET['cfs_wdc_nag_ignore']) && '0' == $_GET['cfs_wdc_nag_ignore'] ) {
+             add_user_meta($user_id, 'cfs_wdc_ignore_notice', 'true', true);
+	}
+}
 
 function slabtext_shortcode( $atts, $content = null ) {
 
@@ -44,16 +85,21 @@ add_shortcode( 'slabtext', 'slabtext_shortcode' );
 
 function slabtextline_shortcode( $atts, $content = null ) {
 
-	$array = array (
-		'<p>[' => '[', 
-		']</p>' => ']', 
-		']<br />' => ']'
-	);
+	$array = shortcode_atts( array (
+		'<p>[' => '[',
+		']</p>' => ']',
+		']<br />' => ']',
+		'font' => '',
+		'transform'  => '',
+		'color' => ''
+
+	), $atts );
 	
+
 	$content = strtr($content, $array);	
 
-	$GLOBALS["SLAB_TEXT_LINE"] .= '"' . $content . '",';
-	
+	$GLOBALS["SLAB_TEXT_LINE"] .= '"<span style=\'color:' . $array['color'] .';text-transform:'. $array['transform'] .';font-family:'. $array['font'] .'\'>' . $content . '</span>",';
+
 	return '';
 }
 add_shortcode( 'slab', 'slabtextline_shortcode' );
